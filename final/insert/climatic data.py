da35c41d31700c742e55bfebbbe408bf69855fc3
BestAwperEver <misher-fsh@yandex.ru>
2017-06-16 02:09:28 +0300

    insert data into db

---
+++ final/insert/climatic data.py
@@ -0,0 +1,35 @@
+import os
+import mysql.connector
+
+files = os.listdir("./2015")
+
+cnx = mysql.connector.connect(user='root', password='Trololo1',
+                              host='127.0.0.1', database='climatic_data')
+cursor = cnx.cursor()
+
+for file in files:
+	file = str(file)
+	f = open("./2015/" + file)
+	query = 'INSERT IGNORE INTO observations VALUES '
+	line_count = 0
+	for line in f:
+		if line_count > 0:
+			query += ','
+		values = line.split()
+		date = values[0] + '-' + values[1] + '-' + values[2]
+		query += '(' + file.split('-')[0] + ',"' + date + '"'
+		for value in values[3:-1]:
+			query += ',' + value
+		query += ')'
+		line_count += 1
+		# if line_count > 1000:
+		# 	line_count = 0
+		# 	query += ';'
+		# 	cursor.execute(query)
+		# 	query = 'INSERT IGNORE INTO observations VALUES '
+	query += ';'
+	cursor.execute(query)
+	cnx.commit()
+	f.close()
+cursor.close()
+cnx.close()
